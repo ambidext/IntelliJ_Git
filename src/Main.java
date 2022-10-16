95861dd0beec78a8bf8ca120f966fc69c41a8086
ambidext <ambidext@naver.com>
2022-10-16 20:37:49 +0900

    Revert "v2.0 test"
    This reverts commit 8850abfc6d41077e7cc444f7c8f9fa2d3de289ae.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world Git Test!");
+<<<<<<< HEAD
         System.out.println("master conflict test ver3.0");
+=======
+        System.out.println("master conflict test");
+>>>>>>> parent of 8850abf (v2.0 test)
     }
 }
