3c6418d0669ce26d86f03d14dbf9540d22e455dd
ambidext <ambidext@naver.com>
2022-10-16 20:38:30 +0900

    ver 4.0

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world Git Test!");
-<<<<<<< HEAD
-        System.out.println("master conflict test ver3.0");
-=======
+        System.out.println("master conflict test ver4.0");
         System.out.println("master conflict test");
->>>>>>> parent of 8850abf (v2.0 test)
     }
 }
