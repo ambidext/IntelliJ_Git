8ecce1813ee8cdfc164925defebdf0167b28c670
ambidext <ambidext@naver.com>
2022-10-16 17:38:40 +0900

    IntelliJ Git Test - conflict test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world Git Test!");
-        System.out.println("branch test");
+        System.out.println("master conflict test");
     }
 }
