b7c9c7e3c55c5d2e6c31065002ba632dcfb0cf57
ambidext <ambidext@naver.com>
2022-10-16 20:37:03 +0900

    ver 3.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world Git Test!");
-        System.out.println("master conflict test0.9");
+        System.out.println("master conflict test ver3.0");
     }
 }
